a387bbe2e4c4d6cf91c1f1cbf95da642966de24b
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-04-02 11:17:52 -0400

    Visualize present/dismiss

--- PushPopPresentDismissVisualized/PushPopPresentDismissVisualized/AppDelegate.swift
+++ PushPopPresentDismissVisualized/PushPopPresentDismissVisualized/AppDelegate.swift
@@ -11,14 +11,32 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     
+    // TODO: Build push/pop options
+    let presentViewController = PresentViewController()
+    let dismissViewController = DismissViewController()
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        presentViewController.delegate = self
+        dismissViewController.delegate = self
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBlue
-        window?.rootViewController = ViewController()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = presentViewController
         
         return true
     }
 }
 
+extension AppDelegate: PresentViewControllerProtocol {
+    func presentPressed() {
+        presentViewController.present(dismissViewController, animated: true)
+    }
+}
+
+extension AppDelegate: DismissViewControllerProtocol {
+    func dismissPressed() {
+        dismissViewController.dismiss(animated: true)
+    }
+}
