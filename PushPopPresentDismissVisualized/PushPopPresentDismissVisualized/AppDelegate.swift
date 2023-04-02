67aa7364cc1d36c9a909ea0b2a281ae2fcb80c0e
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2023-04-02 10:37:05 -0400

    Remove app scene manifest for storyboards

--- PushPopPresentDismissVisualized/PushPopPresentDismissVisualized/AppDelegate.swift
+++ PushPopPresentDismissVisualized/PushPopPresentDismissVisualized/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.makeKeyAndVisible()
-        window?.backgroundColor = .systemBackground
+        window?.backgroundColor = .systemBlue
         window?.rootViewController = ViewController()
         
         return true
