34ff63e3f6ff3e55a65ebcc6ab04310db5baf233
Jeff Sweeney <Jeffrey.Sweeney812.GH@gmail.com>
2024-05-15 07:16:08 -0400

    Project init

---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jeffrey Sweeney on 5/15/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
